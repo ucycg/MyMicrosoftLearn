77d96340d25249389b40c09618eb5ba14af35df7
Justin Cott <jcott@initse.com>
2025-09-11 09:55:38 +0200

    Clean up tracked files according to .gitignore

--- src/ConsoleApps/TestProject/Program.cs
+++ src/ConsoleApps/TestProject/Program.cs
@@ -1,3 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello C#!");
+﻿Random dice = new Random();
+int roll = dice.Next(1, 7);
+Console.WriteLine(roll);
 
+int number = 7;
+string text = "seven";
+
+Console.WriteLine(number);
+Console.WriteLine();
+Console.WriteLine(text);
+Console.WriteLine();
+
+int roll1 = dice.Next();
+int roll2 = dice.Next(101);
+int roll3 = dice.Next(50, 101);
+
+Console.WriteLine($"First roll: {roll1}");
+Console.WriteLine($"Second roll: {roll2}");
+Console.WriteLine($"Third roll: {roll3}");
+
+dice.Next(1,7)
