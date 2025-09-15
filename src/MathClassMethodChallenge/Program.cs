0f37c76fd41eeaae1e013f64171bbb8f74d49802
Justin Cott <jcott@initse.com>
2025-09-15 17:09:18 +0200

    Solve Math class methods challenge

---
+++ src/MathClassMethodChallenge/Program.cs
@@ -0,0 +1,12 @@
+﻿
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int firstValue = 500;
+        int secondValue = 600;
+        int largerValue = int.Max(firstValue, secondValue);
+
+        Console.WriteLine(largerValue);
+    }
+}
