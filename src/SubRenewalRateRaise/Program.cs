80a76be56197f1098f4de2d79e095cd37fe13c75
Justin Cott <ucycg@student.kit.edu>
2025-09-16 17:50:50 +0200

    Finished if-else challenge

---
+++ src/SubRenewalRateRaise/Program.cs
@@ -0,0 +1,25 @@
+﻿Random random = new();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+if (daysUntilExpiration == 0)
+{
+    Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1)
+{
+    Console.WriteLine("Your subscription expires within a day!");
+}
+else if (daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+}
+else if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+
+if (discountPercentage > 0)
+{
+    Console.WriteLine($"Renew now and save {discountPercentage}!");
+}
